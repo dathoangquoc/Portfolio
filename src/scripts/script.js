36800225b6a80a178b42ffd01d306679ead3b7fd
dathoangquoc <dathgqc@gmail.com>
2024-03-13 21:48:15 +0700

    Styling + Work page

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,28 +1,17 @@
-// Shine fx
-
-// Add mousemove listener to all card div
-const cards = document.querySelectorAll(".card");
-cards.forEach(card => {
-    card.addEventListener('mousemove', (e) => {
-        const { x, y } = card.getBoundingClientRect();
-        card.style.setProperty("--x", e.clientX - x);
-        card.style.setProperty("--y", e.clientY - y);
-    })
-});
 
 // Spiral Animation
 // Split each letter into seperate div
-const text = 'eyecatchingthingy';
+const text = 'attention catching thingy';
 const charList = text.split('');
 
 const spiral = document.getElementById("spiral");
 let count = 0;
 let delay = 1000;
-const delayIncrement = 150;
+const delayIncrement = 200;
 
 // Add delay to each div 
 charList.forEach(char => {
-    spiral.insertAdjacentHTML('beforeend', `<div class='spiralChar text-3xl' id='spiralChar${count}'>${char}</div>`);
+    spiral.insertAdjacentHTML('beforeend', `<div class='spiralChar' id='spiralChar${count}'>${char}</div>`);
     document.getElementById(`spiralChar${count}`).style.animationDelay = `${delay}ms`
     delay += delayIncrement;
     count += 1;
@@ -33,7 +22,7 @@ const spiral2 = document.getElementById("spiral2");
 delay = 3000; // second spiral delay = first delay + half animation duration
 
 charList.forEach(char => {
-    spiral2.insertAdjacentHTML('beforeend', `<div class='spiralChar text-3xl' id='spiralChar${count}'>${char}</div>`);
+    spiral2.insertAdjacentHTML('beforeend', `<div class='spiralChar' id='spiralChar${count}'>${char}</div>`);
     document.getElementById(`spiralChar${count}`).style.animationDelay = `${delay}ms`
     delay += delayIncrement;
     count += 1;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,7 @@ export default {
 	theme: {
 		extend: {
 			fontFamily: {
-				"montserrat": ["Montserrat", "sans-serif"]
+				heming: ['Heming', 'monotype'],
 			}
 		},
 	},
